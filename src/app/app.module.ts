5fbe94fe750c0d18dd93b0259e9c40bd07c2e38a
Jack Sherven <jacksherven@yahoo.com>
2022-04-19 16:18:04 -0500

    Components generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TheaterComponent } from './theater/theater.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { SeatSelectorComponent } from './seat-selector/seat-selector.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheaterComponent,
+    MovieCardComponent,
+    MainPageComponent,
+    NavBarComponent,
+    MovieSearchComponent,
+    SearchbarComponent,
+    MovieDetailsComponent,
+    SeatSelectorComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/seat-selector/seat-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-selector',
+  templateUrl: './seat-selector.component.html',
+  styleUrls: ['./seat-selector.component.css']
+})
+export class SeatSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
