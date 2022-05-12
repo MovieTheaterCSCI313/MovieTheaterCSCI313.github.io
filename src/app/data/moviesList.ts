796381a29f426d78ebccc7c267d694b87a1c9f66
Jack Sherven <jacksherven@yahoo.com>
2022-05-11 19:46:57 -0500

    final chagnes to make online work

--- src/app/data/moviesList.ts
+++ src/app/data/moviesList.ts
@@ -5,28 +5,28 @@ export const movies: Movie[] =[
             genre: "Action",
             rating: "PG",
             description: "Spider man who has spider powers does spider things. Wow",
-            imageSrc: "/assets/images/SpoodermanNoHome.jpg"
+            imageSrc: "./assets/images/SpoodermanNoHome.jpg"
         },
         {
             name: "Morbious",
             genre: "Action/Adventure",
             rating: "M",
             description: "He's like a vampire or something I think?",
-            imageSrc: "/assets/images/Morbius.jpg"
+            imageSrc: "./assets/images/Morbius.jpg"
         },
         {
             name : "Sanic",
             genre: "Action/Comedy",
             rating: "PG",
             description: "He is the fast blue hedge boi. He consumes the CHILI DOG",
-            imageSrc: "/assets/images/Sanic.jpg"
+            imageSrc: "./assets/images/Sanic.jpg"
         },
         {
             name: "The bat, man",
             genre: "Action/Adventure",
             rating: "PG-13",
             description: "There is a man who bats around with this bat powers. Riddle question time.",
-            imageSrc: "/assets/images/Batman.jpg"
+            imageSrc: "./assets/images/Batman.jpg"
         },
         {
             name: "Moonfall",
@@ -35,7 +35,7 @@ export const movies: Movie[] =[
             description: "The moon (big white thing in the sky; werewolves use it)" 
             + " has fallen from the sky to the earth. This is not ideal, as thats where" + 
             " most of the people live",
-            imageSrc: "/assets/images/MoonFall.jpg"
+            imageSrc: "./assets/images/MoonFall.jpg"
             //Runtime 2 hrs
         },
         {
@@ -44,7 +44,7 @@ export const movies: Movie[] =[
             rating: "PG-13",
             description: "So theres this alien guy made out of G O O . And theres this other guy" + 
             " who's a reporter. They meet by chance and have a beautiful bro-mance. \n Also, explosions",
-            imageSrc: "/assets/images/Venom.jpg"
+            imageSrc: "./assets/images/Venom.jpg"
         },
         // {
         //     name: "",
